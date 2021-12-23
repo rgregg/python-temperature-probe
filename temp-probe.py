bb310fa892b0550601dac0821b29d03fa9b570ee
Ryan Gregg <ryan+git@ryangregg.com>
2021-12-23 07:21:51 +0000

    Add rotating logs

--- temp-probe.py
+++ temp-probe.py
@@ -8,21 +8,34 @@ import os
 import json
 from time import gmtime, strftime
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import paho.mqtt.client as paho
 import signal
 from threading import Event
 
 class ProbeConfig:
-   def __init__(self):
-      self.broker="192.168.42.65"
-      self.port=1883
-      self.topic="house/temp-probe1/value"
-      self.status_topic="house/temp-probe1/status"
-      self.client_name="control1"
-      self.offline_status="offline"
-      self.online_status="online"
-      self.log_to_console=False
-      self.seconds_between_readings = 60
+   def __init__(self, raw = None):
+      if raw is None:
+         self.broker="192.168.42.65"
+         self.port=1883
+         self.topic="house/temp-probe1/value"
+         self.status_topic="house/temp-probe1/status"
+         self.client_name="control1"
+         self.offline_status="offline"
+         self.online_status="online"
+         self.log_to_console=False
+         self.seconds_between_readings = 60
+         self.log_path = "/home/pi/temp-probe-python/logs/temp-probe.log"
+      else:
+         self.broker = raw['mqtt-broker']['address']
+         self.port = raw['mqtt-broker']['port']
+         self.topic = raw['topic']
+         self.status_topic = raw['status']['topic']
+         self.client_name = raw['name']
+         self.offline_status = raw['status']['offline']
+         self.online_status = raw['status']['online']
+         self.log_to_console = raw['log-to-console']
+         self.seconds_between_readings = raw['seconds-between-readings']
 
 class ProbeReading:
    def __init__(self, device, temperature, humidity):
@@ -40,29 +53,40 @@ class TempProbeReader:
       self.config = config
       self.QOS1 = 1
       self.sensor = None
-      logging.basicConfig(filename='temp-probe.log',level=logging.DEBUG)
-      if config.log_to_console == True:
-         logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+      self.logger = self.enable_logging()
       signal.signal(signal.SIGINT, self.exit_gracefully)
       signal.signal(signal.SIGTERM, self.exit_gracefully)
 
+   def enable_logging(self):
+      logger = logging.getLogger('rotating logs')
+      logger.setLevel(logging.INFO)
+
+      handler = TimedRotatingFileHandler(self.config.log_path,
+                                    when='d',
+                                    interval=1,
+                                    backupCount=7)
+      logger.addHandler(handler)
+      if config.log_to_console:
+         logger.addHandler(logging.StreamHandler(sys.stdout))
+      return logger
+
    def exit_gracefully(self, *args):
-      logging.info(f"Interrupted by {args}")
+      self.logger.info(f"Interrupted by {args}")
       self.exit.set();
 
    def on_publish(self,client,userdata,rc):
-      logging.info("mqtt data published")
+      self.logger.info("mqtt data published")
 
    def on_disconnect(self, client, userdata, rc):
-      logging.debug(f"mqtt on_disconnect {client}, {userdata}, {rc}")
+      self.logger.debug(f"mqtt on_disconnect {client}, {userdata}, {rc}")
       if rc != 0:
-        logging.error("mqtt client disconnected unexpected.")
+        self.logger.error("mqtt client disconnected unexpected.")
       else:
-        logging.info("mqtt client disconnected")
+        self.logger.info("mqtt client disconnected")
 
    def run_loop(self):
       exit = self.exit;
-      logging.debug(f"Exit is set: {exit.is_set()}")
+      self.logger.debug(f"Exit is set: {exit.is_set()}")
       self.init_probe()
       mqttc = self.init_mqtt()
       while not exit.is_set():
@@ -72,7 +96,7 @@ class TempProbeReader:
          self.publish_topic(mqttc, self.config.topic, data)
          # sleep until next iteration of loop
          value = exit.wait(self.config.seconds_between_readings)
-         logging.debug(f"event wait returned {value}")
+         self.logger.debug(f"event wait returned {value}")
       self.publish_topic(mqttc, self.config.status_topic, self.config.offline_status)
       mqttc.disconnect()
       mqttc.loop_stop()
@@ -80,16 +104,16 @@ class TempProbeReader:
    def publish_topic(self, client, topic, data):
       result = client.publish(topic, data, self.QOS1)
       if result.rc == paho.MQTT_ERR_SUCCESS:
-         logging.info(f"MQTT published topic '{topic}' with '{data}'. Result: {result.rc}")
+         self.logger.info(f"MQTT published topic '{topic}' with '{data}'. Result: {result.rc}")
       elif result.rc == paho.MQTT_ERR_NO_CONN:
-         logging.error("MQTT is not connected to a broker.")
+         self.logger.error("MQTT is not connected to a broker.")
          client.reconnect()
       else:
-         logging.error(f"MQTT could not publish the message {result.rc}")
+         self.logger.error(f"MQTT could not publish the message {result.rc}")
 
    def init_mqtt(self):
       config = self.config
-      logging.info("Connecting to mqtt broker")
+      self.logger.info("Connecting to mqtt broker")
       mqttc = paho.Client(config.client_name)
       mqttc.on_publish = self.on_publish
       mqttc.on_disconnect = self.on_disconnect
@@ -102,16 +126,16 @@ class TempProbeReader:
       return mqttc
 
    def init_probe(self):
-      logging.info('Initializing data probe...')
+      self.logger.info('Initializing data probe...')
       self.i2c = busio.I2C(board.SCL, board.SDA)
       self.sensor = adafruit_si7021.SI7021(self.i2c)
 
    def read_probe(self):
       if not self.sensor:
-         logging.error('sensor has not be configured and cannot be read')
+         self.logger.error('sensor has not be configured and cannot be read')
       else:
          next_reading = ProbeReading('si7021', self.sensor.temperature, self.sensor.relative_humidity)
-         logging.info(f"{next_reading.date_time_utc} - temp {next_reading.temp_c:.1f} C / {next_reading.temp_f:.1f} F" \
+         self.logger.info(f"{next_reading.date_time_utc} - temp {next_reading.temp_c:.1f} C / {next_reading.temp_f:.1f} F" \
                       f" - humidity {next_reading.relative_humidity:.1f} %")
          return next_reading
 
@@ -120,7 +144,7 @@ if __name__ == '__main__':
    config = ProbeConfig()
    reader = TempProbeReader(config)
    reader.run_loop()
-   logging.info(f"TempProbeReader exited gracefully.")
+   reader.logger.info(f"TempProbeReader exited gracefully.")
 
 
 
