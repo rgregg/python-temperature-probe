54a13f91ff0eff934ea78e64215d471c1989b2eb
Ryan Gregg <github@ryangregg.com>
2021-12-22 21:46:38 -0800

    Refactored

--- read-data.py
+++ read-data.py
@@ -10,10 +10,7 @@ from time import gmtime, strftime
 import logging
 import paho.mqtt.client as paho
 import signal
-
-#enable logging
-logging.basicConfig(filename='temp-probe.log',level=logging.DEBUG)
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+from threading import Event
 
 class ProbeConfig:
    def __init__(self):
@@ -24,75 +21,92 @@ class ProbeConfig:
       self.client_name="control1"
       self.offline_status="offline"
       self.online_status="online"
+      self.log_to_console=False
+      self.seconds_between_readings = 60
 
 class GracefulKiller:
   kill_now = False
   def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self, *args):
-    self.kill_now = True
-
-if __name__ == '__main__':
-   killer = GracefulKiller()
-   while not killer.kill_now:
-      
-
-config=ProbeConfig()
-QOS1=1
-
-
-
-
-def on_publish(client,userdata,rc):
-   logging.info("mqtt data published")
-
-def on_disconnect(client, userdata, rc):
-   logging.info("mqtt client disconnected");
-
-
-# Configure the client
-logging.info("connecting to mqtt broker")
-client1 = paho.Client(config.client_name)
-client1.on_publish = on_publish
-client1.on_disconnect = on_disconnect
-# Configure LWT message for unexpected disconnect
-client1.will_set(config.status_topic, config.offline_status, QOS1, retain=False)
-# Connect to broker
-client1.connect(config.broker,config.port)
-ret = client1.publish(config.status_topic, config.online_status, QOS1)
-
-
-time_between_readings_seconds = 60
-logging.info('Initializing data probe...')
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_si7021.SI7021(i2c)
 
 class ProbeReading:
-    def __init__(self, device, temperature, humidity):
+   def __init__(self, device, temperature, humidity):
         self.device = device
         self.temp_c = temperature
         self.temp_f = 32 + (temperature * 9 / 5)
         self.relative_humidity = humidity
         self.date_time_utc = datetime.datetime.utcnow().isoformat()
+   def json(self):
+      return json.dumps(self.__dict__)
 
-# Main loop for reading temperature data
-while True:
-    next_reading = ProbeReading('si7021', sensor.temperature, sensor.relative_humidity)
-    data = json.dumps(next_reading.__dict__)
+class TempProbeReader:
+   def __init__(self, config):
+      self.exit = Event()
+      self.config = config
+      self.killer = GracefulKiller()
+      self.QOS1 = 1
+      self.sensor = None
+      logging.basicConfig(filename='temp-probe.log',level=logging.DEBUG)
+      if config.log_to_console == True:
+         logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+      signal.signal(signal.SIGINT, self.exit_gracefully)
+      signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-    logging.info(f"{next_reading.date_time_utc} - temp {next_reading.temp_c:.1f} C / {next_reading.temp_f:.1f} F" \
-                 f" - humidity {next_reading.relative_humidity:.1f} %")
+   def exit_gracefully(self, *args):
+      logging.info(f"Interrupted by {args}")
+      self.exit.set();
 
-#    logging.info("Temperature: %0.1f C / %0.1f F" % (next_reading.temp_c, next_reading.temp_f))
-#    logging.info("Humidity: %0.1f %%" % next_reading.relative_humidity)
-#    logging.info("Date/Time: %s" % next_reading.date_time_utc)
-#    logging.info("Sensor: %s" % next_reading.device)
+   def on_publish(client,userdata,rc):
+      logging.info("mqtt data published")
 
-    ret = client1.publish(config.topic, data, QOS1)
-    logging.info(f"mqtt publish result {ret.rc}")
+   def on_disconnect(client, userdata, rc):
+      logging.info("mqtt client disconnected")
 
-    time.sleep(time_between_readings_seconds)
+   def run_loop(self):
+      exit = self.exit;
+      self.init_probe()
+      self.init_mqtt()
+      while not exit.is_set:
+         # implement main loop here
+         reading = self.read_probe()
+         data = reading.json()
+         self.publish_topic(self.config.topic, data)
+         # sleep until next iteration of loop
+         exit.wait(self.config.seconds_between_readings)
+
+   def publish_topic(self, topic, data):
+      result = self.client.publish(topic, data, self.QOS1)
+      logging.info(f"MQTT published. Result: {result.rc}")
+
+   def init_mqtt(self):
+      config = self.config
+      logging.info("Connecting to mqtt broker")
+      client1 = paho.Client(config.client_name)
+      client1.on_publish = self.on_publish
+      client1.on_disconnect = self.on_disconnect
+      # Configure LWT message for unexpected disconnect
+      client1.will_set(config.status_topic, config.offline_status, self.QOS1, retain=False)
+      # Connect to broker
+      client1.connect(config.broker,config.port)
+      ret = client1.publish(config.status_topic, config.online_status, self.QOS1)
+
+   def init_probe(self):
+      logging.info('Initializing data probe...')
+      self.i2c = busio.I2C(board.SCL, board.SDA)
+      self.sensor = adafruit_si7021.SI7021(self.i2c)
+
+   def read_probe(self):
+      if not self.sensor:
+         logging.error('sensor has not be configured and cannot be read')
+      else:
+         next_reading = ProbeReading('si7021', self.sensor.temperature, self.sensor.relative_humidity)
+         logging.info(f"{next_reading.date_time_utc} - temp {next_reading.temp_c:.1f} C / {next_reading.temp_f:.1f} F" \
+                      f" - humidity {next_reading.relative_humidity:.1f} %")
+         return next_reading
 
 
+if __name__ == '__main__':
+   config = ProbeConfig()
+   reader = TempProbeReader(config)
+   reader.run_loop()
+   logging.info(f"completed gracefully.")
