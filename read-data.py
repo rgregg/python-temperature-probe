5a5abd9f2067883fe38b48b5c7749fe84dcc8f45
Ryan Gregg <ryan+git@ryangregg.com>
2021-12-23 06:08:07 +0000

    Updated to use MQTT broker for publishing readings to a topic

--- read-data.py
+++ read-data.py
@@ -21,13 +21,8 @@ class ProbeConfig:
       self.client_name="control1"
       self.offline_status="offline"
       self.online_status="online"
-      self.log_to_console=False
+      self.log_to_console=True
       self.seconds_between_readings = 60
-
-class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-
 
 class ProbeReading:
    def __init__(self, device, temperature, humidity):
@@ -43,7 +38,6 @@ class TempProbeReader:
    def __init__(self, config):
       self.exit = Event()
       self.config = config
-      self.killer = GracefulKiller()
       self.QOS1 = 1
       self.sensor = None
       logging.basicConfig(filename='temp-probe.log',level=logging.DEBUG)
@@ -56,39 +50,56 @@ class TempProbeReader:
       logging.info(f"Interrupted by {args}")
       self.exit.set();
 
-   def on_publish(client,userdata,rc):
+   def on_publish(self,client,userdata,rc):
       logging.info("mqtt data published")
 
-   def on_disconnect(client, userdata, rc):
-      logging.info("mqtt client disconnected")
+   def on_disconnect(self, client, userdata, rc):
+      logging.debug(f"mqtt on_disconnect {client}, {userdata}, {rc}")
+      if rc != 0:
+        logging.error("mqtt client disconnected unexpected.")
+      else:
+        logging.info("mqtt client disconnected")
 
    def run_loop(self):
       exit = self.exit;
+      logging.debug(f"Exit is set: {exit.is_set()}")
       self.init_probe()
-      self.init_mqtt()
-      while not exit.is_set:
+      mqttc = self.init_mqtt()
+      while not exit.is_set():
          # implement main loop here
          reading = self.read_probe()
          data = reading.json()
-         self.publish_topic(self.config.topic, data)
+         self.publish_topic(mqttc, self.config.topic, data)
          # sleep until next iteration of loop
-         exit.wait(self.config.seconds_between_readings)
+         value = exit.wait(self.config.seconds_between_readings)
+         logging.debug(f"event wait returned {value}")
+      self.publish_topic(mqttc, self.config.status_topic, self.config.offline_status)
+      mqttc.disconnect()
+      mqttc.loop_stop()
 
-   def publish_topic(self, topic, data):
-      result = self.client.publish(topic, data, self.QOS1)
-      logging.info(f"MQTT published. Result: {result.rc}")
+   def publish_topic(self, client, topic, data):
+      result = client.publish(topic, data, self.QOS1)
+      if result.rc == paho.MQTT_ERR_SUCCESS:
+         logging.info(f"MQTT published topic '{topic}' with '{data}'. Result: {result.rc}")
+      elif result.rc == paho.MQTT_ERR_NO_CONN:
+         logging.error("MQTT is not connected to a broker.")
+         client.reconnect()
+      else:
+         logging.error(f"MQTT could not publish the message {result.rc}")
 
    def init_mqtt(self):
       config = self.config
       logging.info("Connecting to mqtt broker")
-      client1 = paho.Client(config.client_name)
-      client1.on_publish = self.on_publish
-      client1.on_disconnect = self.on_disconnect
-      # Configure LWT message for unexpected disconnect
-      client1.will_set(config.status_topic, config.offline_status, self.QOS1, retain=False)
+      mqttc = paho.Client(config.client_name)
+      mqttc.on_publish = self.on_publish
+      mqttc.on_disconnect = self.on_disconnect
       # Connect to broker
-      client1.connect(config.broker,config.port)
-      ret = client1.publish(config.status_topic, config.online_status, self.QOS1)
+      mqttc.connect(config.broker,config.port)
+      mqttc.loop_start()
+      ret = mqttc.publish(config.status_topic, config.online_status, self.QOS1)
+      # Configure LWT message for unexpected disconnect
+      mqttc.will_set(config.status_topic, config.offline_status, self.QOS1)
+      return mqttc
 
    def init_probe(self):
       logging.info('Initializing data probe...')
@@ -109,4 +120,4 @@ if __name__ == '__main__':
    config = ProbeConfig()
    reader = TempProbeReader(config)
    reader.run_loop()
-   logging.info(f"completed gracefully.")
+   logging.info(f"TempProbeReader exited gracefully.")
